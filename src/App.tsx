75a9383b45a7cc53128162e54be48856fc9a0cda
Vasya Rymarenko <vrima60@gmail.com>
2023-02-27 18:44:52 +0200

    workflow test

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export const App: React.FC = () => {
   return (
     <div className="starter">
       <Provider onClick={() => ({})}>
-        <h1>Hello world!</h1>
+        <h1>testing deploy</h1>
       </Provider>
     </div>
   );
